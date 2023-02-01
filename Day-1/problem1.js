f4853f66465fac691631dacf38fceda0763ed3bb
shaykot43500 <hossainshaykot@gmail.com>
2023-02-01 10:09:17 +0600

    number of problem 1 added

--- Day-1/problem1.js
+++ Day-1/problem1.js
@@ -1,3 +1,23 @@
-function harysmom(){
-    console.log(23);
-}
+                // Number of Problem : 1;
+/*
+
+Harry’s mom gave him money and asked him to buy some oranges and apples. Write a program to
+help Harry calculate how much money the shopkeeper will return.
+
+Input:
+The first line of the input is the taka Harry’s mom gave him.
+The second line is the cost of 1 kg of oranges and 1 kg of apples.
+
+                Sample Input:   1000
+                                700
+                    Output:     300
+
+*/
+
+                let harrysMomgive = 1000;
+                let priceOrange = 450;
+                let priceApple = 250;
+                let totalExpense = priceOrange + priceApple;
+
+            let shopkeeperReaturns = harrysMomgive - totalExpense;
+            console.log(shopkeeperReaturns);// shopkeeperReaturns: 300   
