e1c7634f71c9911097ec011e83b748bb32fd35de
shaykot43500 <hossainshaykot@gmail.com>
2023-02-01 12:00:19 +0600

    added new

--- Day-2/practicsProblem2.js
+++ Day-2/practicsProblem2.js
@@ -11,3 +11,100 @@
 
     Write a program to find your and your friends’ grades using if-else.
  */
+         // My Score
+
+    let myScore = 90; 
+    
+    if( myScore >= 80){
+       console.log('My Score:','A Grade');
+    }
+    else if(myScore >= 60){
+         console.log('My Score:','B Grade');
+    }
+    else if(myScore >= 50){
+         console.log('My Score:','C Grade');
+    }
+    else if(myScore >= 40){
+      console.log('My Score:','D Grade');
+    }
+    else {
+      console.log('My Score:','F Grade');
+    }
+
+                  // tom Score 
+    let tomsScore = 66;
+    
+    if( tomsScore >= 80){
+      console.log('Tom Score:','A Grade');
+   }
+   else if(tomsScore >= 60){
+        console.log('Tom Score:','B Grade');
+   }
+   else if(tomsScore >= 50){
+        console.log('Tom Score:','C Grade');
+   }
+   else if(tomsScore >= 40){
+     console.log('Tom Score:','D Grade');
+   }
+   else {
+     console.log('Tom Score:','F Grade');
+   }
+
+
+               // Janes Score
+    let janesScore = 95;
+    if( janesScore >= 80){
+      console.log('Jane Score:','A Grade');
+   }
+   else if(janesScore >= 60){
+        console.log('Jane Score:','B Grade');
+   }
+   else if(janesScore >= 50){
+        console.log('Jane Score:','C Grade');
+   }
+   else if(janesScore >= 40){
+     console.log('Jane Score:','D Grade');
+   }
+   else {
+     console.log('Jane Score:','F Grade');
+   }
+
+
+               // Peter Score
+
+   let peterScore = 56;
+    if( peterScore >= 80){
+      console.log('Peter Score:','A Grade');
+   }
+   else if(peterScore >= 60){
+        console.log('Peter Score:','B Grade');
+   }
+   else if(peterScore >= 50){
+        console.log('Peter Score:','C Grade');
+   }
+   else if(peterScore >= 40){
+     console.log('Peter Score:','D Grade');
+   }
+   else {
+     console.log('Peter Score:','F Grade');
+   }
+
+            
+            // Johns Score
+
+    let johnsScore = 40;
+    if( johnsScore >= 80){
+      console.log('John Score: ','A Grade');
+   }
+   else if(johnsScore >= 60){
+        console.log('John Score: ','B Grade');
+   }
+   else if(johnsScore >= 50){
+        console.log('John Score: ','C Grade');
+   }
+   else if(johnsScore >= 40){
+     console.log('John Score: ','D Grade');
+   }
+   else {
+     console.log('John Score: ','F Grade');
+   }
